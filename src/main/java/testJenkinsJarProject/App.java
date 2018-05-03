2f67b4be6715b56ab827a49c18092a50a84dcd93
yangze8818 <yangze_lmkj@163.com>
2018-05-03 18:07:51 +0800

    Jenkins Build Jar 项目测试

---
+++ src/main/java/testJenkinsJarProject/App.java
@@ -0,0 +1,13 @@
+package testJenkinsJarProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
